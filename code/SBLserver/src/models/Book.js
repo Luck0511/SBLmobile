320b6194b2534eaec4f352909cda2d4c2ce493e4
Natalia <nataliabusuioc27@gmail.com>
2025-11-20 16:21:19 +0100

    - created table model for 'book'

--- code/SBLserver/src/models/Book.js
+++ code/SBLserver/src/models/Book.js
@@ -1,1 +1,53 @@
-//model class for book's db table
+//MODEL CLASS FOR BOOK'S DB TABLE
+
+//import sequelize instance
+import {sequelize} from "../config/dbConfig.js";
+
+//import data types
+import {DataTypes, Model} from "sequelize";
+
+//book table definition
+export class Book extends Model {}
+
+Book.init(
+    {
+    bookID: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    genre: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+    },
+    titolo: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+    },
+    author: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    publication_year:{
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    isbn:{
+        type: DataTypes.STRING(13),
+        allowNull: true,
+    },
+    cover_url:{
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+},
+    {
+        sequelize,
+        modelName: "Book",
+        timestamps: false,
+    }
+);
