0265e5c3a6a11344100f1a97361f8e818e23d5c1
Natalia <nataliabusuioc27@gmail.com>
2025-11-20 16:42:10 +0100

    - created junction table model for 'user_book'

--- code/SBLserver/src/models/User_book.js
+++ code/SBLserver/src/models/User_book.js
@@ -1,1 +1,38 @@
-//junction db table for 'user' and 'book'
+//JUNCTION DB TABLE FOR 'USER' AND 'BOOK'
+
+//import sequelize instance
+import {sequelize} from "../config/dbConfig.js";
+
+//import data types
+import {DataTypes, Model} from "sequelize";
+
+//user_book junction definition
+export class UserBook extends Model {}
+
+UserBook.init(
+    {
+        userID:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+        },
+        bookID:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+        },
+        times_read:{
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            defaultValue: 0,
+        },
+        last_read_at:{
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+},
+    {
+        sequelize,
+        modelName: "user_book",
+        timestamps: false,
+    })
