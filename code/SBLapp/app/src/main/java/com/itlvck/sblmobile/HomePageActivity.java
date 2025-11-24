f82a14e8517393dea84e1012de5799cdbe09268c
Luc <lucaterra05@gmail.com>
2025-11-24 16:42:44 +0100

    NEW in SBLapp: simple check for user log in status

--- code/SBLapp/app/src/main/java/com/itlvck/sblmobile/HomePageActivity.java
+++ code/SBLapp/app/src/main/java/com/itlvck/sblmobile/HomePageActivity.java
@@ -10,15 +10,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HomePageActivity extends AppCompatActivity {
 
+    public static boolean isLoggedIn = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.homepage);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.homePageView), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        if(isLoggedIn) {
+            //if user is logged in, show homepage
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.homepage);
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.homePageView), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }else{
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.login);
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
     }
 }
