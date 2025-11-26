d1c8578c74ecda8c2b692dbd60c6f2a2786a4599
Natalia <nataliabusuioc27@gmail.com>
2025-11-26 17:28:13 +0100

    Refactor: Convert login and registration to Fragments - Created AuthActivity as a container - Implemented LoginFragment (NOT FINISHED) - Extracted base layout (fragment_auth_base) with shared logo - Separated specific content (content_login, content_register) - Added navigation between fragmentsing a file

--- code/SBLapp/app/src/main/java/com/itlvck/sblmobile/activity/AuthActivity.java
+++ code/SBLapp/app/src/main/java/com/itlvck/sblmobile/activity/AuthActivity.java
@@ -1,4 +1,22 @@
 package com.itlvck.sblmobile.activity;
 
-public class AuthActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.itlvck.sblmobile.R;
+import com.itlvck.sblmobile.fragment.auth.LoginFragment;
+
+
+public class AuthActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_auth);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.auth_content_container, new LoginFragment())
+                    .commit();
+        }
+    }
 }
