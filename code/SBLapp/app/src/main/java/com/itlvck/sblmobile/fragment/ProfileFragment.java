bfb734de1ac5cc2604b8448a03a1c19cd256f37e
Natalia <nataliabusuioc27@gmail.com>
2025-11-27 17:49:43 +0100

    NEW in SBLapp: - created the logic in ResearchFragment.java (Not Completed) - made small changes (fixed problems) - Added logic in ProfileFragment.java for ListsFragment.java (Not Completed) - created the logic in ListsFragment.java (Not Completed) - created layout fragment_lists.xml

--- code/SBLapp/app/src/main/java/com/itlvck/sblmobile/fragment/ProfileFragment.java
+++ code/SBLapp/app/src/main/java/com/itlvck/sblmobile/fragment/ProfileFragment.java
@@ -1,22 +1,24 @@
 package com.itlvck.sblmobile.fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.itlvck.sblmobile.R;
 
 public class ProfileFragment extends Fragment {
     //Variables
+    private ImageButton btnGoToLists;
 
     public ProfileFragment(){
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -25,9 +27,28 @@ public class ProfileFragment extends Fragment {
 
         //Here it goes the layout of the fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //Initialisation variables
+        btnGoToLists = view.findViewById(R.id.btnGoToLists); //Initializing GoToLists Button
+
+        //Click Event management
+        btnGoToLists.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToListsFragment();
+            }
+        });
+
         return view;
     }
 
-    //Creating the logic for the login submit (not finished)
-    private void loginSubmit(){}
+    //Method to navigate to the lists
+    private void navigateToListsFragment() {
+        ListsFragment listsFragment = new ListsFragment();
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, listsFragment);
+        transaction.addToBackStack(null); // u can turn back with the "back" Button
+        transaction.commit();
+    }
+
 }
--- code/SBLapp/app/src/main/java/com/itlvck/sblmobile/fragment/ResearchFragment.java
+++ code/SBLapp/app/src/main/java/com/itlvck/sblmobile/fragment/ResearchFragment.java
@@ -1,6 +1,30 @@
 package com.itlvck.sblmobile.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
+import com.itlvck.sblmobile.R;
+
 public class ResearchFragment extends Fragment {
+    public ResearchFragment(){
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        //Here it goes the layout of the fragment
+        View view = inflater.inflate(R.layout.fragment_research, container, false);
+        return view;
+    }
+
+    //Creating the logic for the research (not finished)
+    private void researchView(){}
 }
+
